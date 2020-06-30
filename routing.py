9eac78ef8ab0d8e46c5386b5c6d882d50c6c8fae
blake <blake.anthony.smith@gmail.com>
2020-06-30 12:25:00 -0700

    added a routing service to resolve the master ip address

---
+++ routing.py
@@ -0,0 +1,26 @@
+"""A super simple routing service that will direct android clients to the current cluster leader. 
+It's unsecure AF and will need some serious updating later """
+import flask
+import json
+from pathlib import Path
+
+app = flask.Flask("Routing Service")
+
+infoFile = Path("masterinfo.json")
+
+@app.route("/update/<ip>/<port>")
+def updateMaster(ip, port):
+    with infoFile.open('w') as info:
+        json.dump(dict(ip=ip, port=port), info)
+
+    return dict(result=True, address=(ip, port))
+
+@app.route("/masterAddress")
+def whoIsMaster():
+    with infoFile.open('r') as info:
+        return json.load(info)
+
+if __name__ == "__main__":
+    app.run(port=9696)
+
+
