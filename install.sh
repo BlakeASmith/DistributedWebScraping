0eb8cdff39518a2f51e4314e2949df135db0390d
Michail Roesli <misha.roesli@gmail.com>
2020-07-06 16:01:07 -0700

    remove -u and add proto install

--- install.sh
+++ install.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 export GO111MODULE=on  # Enable module mode
-go get -u -v google.golang.org/grpc
-go get -u -v google.golang.org/genproto/googleapis/rpc/status
-go get -u -v github.com/golang/protobuf/protoc-gen-go
-go get -u -v github.com/golang/protobuf/protoc-gen-go-grpc
-go get -u -v google.golang.org/grpc
-go get -u -v golang.org/x/sys/unix
-go get -u -v golang.org/x/text/secure/bidirule
-go get -u -v  golang.org/x/text/unicode/bidi
-go get -u -v golang.org/x/text/unicode/norm
-
-
+go get -v google.golang.org/grpc
+go get -v google.golang.org/genproto/googleapis/rpc/status
+go get -v github.com/golang/protobuf/protoc-gen-go
+go get -v github.com/golang/protobuf/protoc-gen-go-grpc
+go get -v google.golang.org/grpc
+go get -v golang.org/x/sys/unix
+go get -v golang.org/x/text/secure/bidirule
+go get -v golang.org/x/text/unicode/bidi
+go get -v golang.org/x/text/unicode/norm
+go get -v github.com/golang/protobuf/proto
