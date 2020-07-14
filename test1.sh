fa03faf4d1dfa7f52886f54472d2ea92ffa483ca
blake <blake.anthony.smith@gmail.com>
2020-07-13 23:32:35 -0700

    added gradle build to vc

--- test1.sh
+++ test1.sh
@@ -5,6 +5,7 @@ sleep 1 # give some time for server to start
 java -jar ./DistributedWebScraper/build/DistributedWebScraper-1.0-SNAPSHOT.jar > /dev/null& client1=$!
 java -jar ./DistributedWebScraper/build/DistributedWebScraper-1.0-SNAPSHOT.jar > /dev/null& 
 java -jar ./DistributedWebScraper/build/DistributedWebScraper-1.0-SNAPSHOT.jar > /dev/null& 
+java -jar ./DistributedWebScraper/build/DistributedWebScraper-1.0-SNAPSHOT.jar > /dev/null& 
 
 trap "kill -9 $client1 $client2 $server $routing" SIGINT # make sure child proccesses killed on CTRL+c
 wait $client1 # keep active until clients finish
