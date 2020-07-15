1ed24d92a0b8706011a61f15fd7c6c096bd66467
Michail Roesli <misha.roesli@gmail.com>
2020-07-15 12:59:45 -0700

    add comments

--- src/run.sh
+++ src/run.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
+# generate proto files
 protoc \
   --proto_path=../proto \
   --go_out=proto \
   --go-grpc_out=proto \
   $(find ../proto -iname "*.proto")
 
+# build gRPC files
 go build proto/*.go
+
+# build the execution components for the server
 go build main.go server.go scraping.go
+
+# execute the server
 go run main.go server.go scraping.go
