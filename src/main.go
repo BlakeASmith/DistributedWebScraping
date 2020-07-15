a575e92a795891c22cd080a51fc7e49c10c5d6e4
Michail Roesli <misha.roesli@gmail.com>
2020-07-15 12:59:14 -0700

    update to local host ip and format

--- src/main.go
+++ src/main.go
@@ -1,18 +1,16 @@
 package main
 
-
-
 func main() {
 	urlchan := make(chan string)
-	seen := make(map[string] bool)
+	seen := make(map[string]bool)
 	go crawl("https://www.usedvictoria.com", "", urlchan, 7, seen)
 	jobs := makeJobChannel(urlchan, 10)
 
-	RoutingServiceAddress :=  Address { IP: "0.0.0.0", Port: 5000 }
+	RoutingServiceAddress := Address{IP: "0.0.0.0", Port: 5000}
 	server := Server{
-		IPAddress: Address{ IP:"192.168.1.147", Port: 6969 },
+		IPAddress:             Address{IP: "0.0.0.0", Port: 6969},
 		RoutingServiceAddress: RoutingServiceAddress,
-		JobChannel: jobs,
+		JobChannel:            jobs,
 	}
 	server.sendUpdateToRoutingService()
 	server.start()
