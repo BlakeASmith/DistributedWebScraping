0f02d1942251c6f1e6eee9a0b23c2cbb62151f74
blake <blake.anthony.smith@gmail.com>
2020-07-16 11:53:34 -0700

    updated RoutingServiceAddress to be of type string

--- src/main.go
+++ src/main.go
@@ -1,14 +1,19 @@
 package main
 
+import "os"
+
 func main() {
+	args := os.Args[1:]
+
+
 	urlchan := make(chan string)
 	seen := make(map[string]bool)
-	go crawl("https://www.usedvictoria.com", "", urlchan, 7, seen)
+	go crawl("https://www.usedvictoria.com", "", urlchan, 30, seen)
 	jobs := makeJobChannel(urlchan, 10)
 
 	RoutingServiceAddress :=  "http://blakesmith.pythonanywhere.com"
 	server := Server{
-		IPAddress: Address{ IP:"127.0.0.1", Port: 6969 },
+		IPAddress: Address{ IP:args[0], Port: 6969 },
 		RoutingServiceAddress: RoutingServiceAddress,
 		JobChannel:            jobs,
 	}
