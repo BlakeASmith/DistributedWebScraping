986556496a133e2d1fa4cab576bdd6751995896e
blake <blake.anthony.smith@gmail.com>
2020-07-15 20:59:57 -0700

    updated link.sh to add proto files to database module

--- link.sh
+++ link.sh
@@ -3,6 +3,7 @@
 
 # symlink proto files into each project
 (cd ./DistributedWebScraper/src/main/proto; ln -f ../../../../proto/* .)
+(cd ./DistributedWebScraper/database/src/main/proto; ln -f ../../../../../proto/* .)
 (cd ./DistributedWebScraping-Android/app/src/main/proto/; ln -f ../../../../../proto/* .)
 
 # symlink shared files into each project, have to cd into the appropriate directory for symlinks to work
