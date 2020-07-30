b36066f85416bff2052fd8b4d1de9b03f977f966
blake <blake.anthony.smith@gmail.com>
2020-07-30 01:28:51 -0700

    moved PluginProvider to provider

--- plugins/PluginProvider/src/main/kotlin/TestPlugin.kt
+++
@@ -1,8 +0,0 @@
-import csc.distributed.webscraper.plugins.Plugin
-import org.jsoup.nodes.Document
-
-class TestPlugin: Plugin {
-    override fun scrape(doc: Document): String {
-        return "{\"test\":\"${doc.location()}\"}"
-    }
-}
