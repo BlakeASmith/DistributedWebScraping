7101ab1bda74d9494d07507d1cf465c0a4042b63
blake <blake.anthony.smith@gmail.com>
2020-07-30 00:37:26 -0700

    removed DistributedWebscraperProject as it has been replaced by new client project

--- DistributedWebScraper/src/main/kotlin/Http.kt
+++
@@ -1,6 +0,0 @@
-import kotlinx.coroutines.Deferred
-
-interface HttpRequestHandler<RESPONSE> {
-    suspend fun <R> get(url: String, action: suspend (RESPONSE) -> R ): R?
-}
-
--- DistributedWebScraper/src/main/kotlin/config.kt
+++
@@ -1,7 +0,0 @@
-
-// TODO: do all configuration using BuildConfig
-object Configuration{
-    const val routingServiceAddress = "http://blakesmith.pythonanywhere.com"
-}
-
-
